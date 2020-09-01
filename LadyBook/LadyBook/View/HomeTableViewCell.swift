346770b007276d19d4c36794915d143d69568e5c
AyanoYata <ayanovoice@yahoo.co.jp>
2020-09-01 18:15:01 +0900

    second commit

---
+++ LadyBook/LadyBook/View/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  LadyBook
+//
+//  Created by AyanoYata on 2020/08/31.
+//  Copyright © 2020 Ayano Yata. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
